5881207bf1401624719489e9d7d9a82342370b70
srwatlanta <shawnrwheeler@gmail.com>
2019-09-16 19:34:06 -0500

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const QuoteCard = (props) =>
+const QuoteCard = ({quote, removeQuote, upvoteQuote, downvoteQuote}) =>
   <div>
     <div className="card card-inverse card-success card-primary mb-3 text-center">
       <div className="card-block">
         <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
+          {quote.content}
+          <footer>- author <cite title="Source Title">{quote.author}</cite></footer>
         </blockquote>
       </div>
       <div className="float-right">
@@ -14,10 +14,12 @@ const QuoteCard = (props) =>
           <button
             type="button"
             className="btn btn-primary"
+            onClick={() => upvoteQuote(quote.id)}
           >
             Upvote
           </button>
           <button
+            onClick={() => downvoteQuote(quote.id)}
             type="button"
             className="btn btn-secondary"
           >
@@ -25,12 +27,13 @@ const QuoteCard = (props) =>
           </button>
           <button
             type="button"
+            onClick={() => removeQuote(quote.id)}
             className="btn btn-danger"
           >
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
+        {quote.votes}
       </div>
     </div>
   </div>;
